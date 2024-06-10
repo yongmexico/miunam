176c4cbd9592b5c02742f96d85711726534d3846
miunam2 <yongmexico@gmail.com>
2024-06-10 17:50:17 +0900

    change description of meta name considering Search Engine Optimization

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = 'Miunam'
-export const SITE_DESCRIPTION = 'Para todos los mexicanos'
+export const SITE_DESCRIPTION = 'MIUNAM es un proyecto desarrollado en México para reducir la brecha digital, mejorar el uso de computadoras y educar sobre software y tecnología.'
 export const HOMEPAGE_ARTICLE_LIMIT = 9;
 export const ARTICLES_PER_PAGE = 9;
